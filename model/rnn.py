033bbff980f882376f23783507c2e5de032b0817
Si-Yuan Huang <goamateur@outlook.com>
2024-11-02 04:02:13 +0800

    added dropout, scheduler, saving dir

--- model/rnn.py
+++ model/rnn.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 # from utils.parse_data import CharParser
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, dropout):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
-        self.rnn_block = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.rnn_block = nn.RNN(input_size, hidden_size, batch_first=True, dropout=dropout)
         self.h2o = nn.Linear(hidden_size, output_size)
         # self.softmax = nn.LogSoftmax(dim=-1)
 
